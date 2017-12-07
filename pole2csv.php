b2296932288a1188afbf546b5a312a28e9a9f7a1
Kaj-Michael Lang <milang@tal.org>
2017-12-07 16:58:22 +0200

    Remove old data on import. Fix parsing of teacher part.

--- pole2csv.php
+++ pole2csv.php
@@ -13,6 +13,7 @@ $a=array();
 $t=array();
 $db=new PDO('sqlite:pole.db');
 
+// Create tables
 $db->exec("CREATE TABLE IF NOT EXISTS history (class text,date varchar, time varchar, place text, who text);");
 $db->exec("CREATE UNIQUE INDEX IF NOT EXISTS history_index on history (class,date,time);");
 
@@ -22,18 +23,20 @@ if (!$fp)
 
 fputcsv($fp, array('Month','Day','WD','Class','Date','Time','Where'));
 
+// Remove old
+$db->exec("DELETE FROM history");
+
 for ($i=0;$i<=$c;$i++) {
  if (($i % 7==0) && ($i>0)) {
 //  print_r($a);
   fputcsv($fp, $a);
 
+  // Parse "Class name - Who"
   $sep=strrpos($a[3], '-');
   $cl=substr($a[3], 0, $sep-1);
-  $wl=substr($a[3], $sep);
-  $a['who']=$wl;
-  $a['class']=$cl;
-
-  // print_r($a);
+  $wl=substr($a[3], $sep+2);
+  $a['who']=trim($wl);
+  $a['class']=trim($cl);
 
   $s = $db->prepare("INSERT INTO history (class, who, date, time, place) VALUES (:c, :w, :d, :t, :p)");
   $s->bindParam(':c', $cl);
